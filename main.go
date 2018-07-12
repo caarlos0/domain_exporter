2a102c4d6432d69feb7a5fc63b709848243d0633
Ales Komarek <ales.komarek@newt.cz>
2018-07-12 13:45:28 +0200

    Added cache, returning timestamp instead of number of days.

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"net/http"
 	"regexp"
 	"strings"
@@ -10,6 +9,7 @@ import (
 
 	"github.com/alecthomas/kingpin"
 	"github.com/domainr/whois"
+	"github.com/patrickmn/go-cache"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/log"
@@ -20,7 +20,9 @@ var (
 	debug   = kingpin.Flag("debug", "show debug logs").Default("false").Bool()
 	version = "master"
 
-	re = regexp.MustCompile(`(?i)(Registry Expiry Date|paid-till|Expiration Date|Expiry.*|expires.*|Expires):\s+(.*)`)
+	storage = cache.New(60*time.Minute, 120*time.Minute)
+
+	re = regexp.MustCompile(`(?i)(Registry Expiry Date|paid-till|Expiration Date|Expiry.*|expires.*|Expires|expire):\s+(.*)`)
 
 	formats = []string{
 		time.ANSIC,
@@ -35,6 +37,7 @@ var (
 		time.RFC3339Nano,
 		"20060102",               // .com.br
 		"2006-01-02",             // .lt
+		"02.01.2006",             // .cz
 		"2006-01-02 15:04:05-07", // .ua
 	}
 )
@@ -46,10 +49,10 @@ func main() {
 
 	if *debug {
 		_ = log.Base().SetLevel("debug")
-		log.Debug("enabled debug mode")
+		log.Debug("debug mode enabled")
 	}
 
-	log.Info("starting domain_exporter", version)
+	log.Info("starting domain_exporter:", version)
 
 	http.Handle("/metrics", promhttp.Handler())
 	http.HandleFunc("/probe", probeHandler)
@@ -67,62 +70,77 @@ func main() {
 			`,
 		)
 	})
-	log.Info("listening on", *bind)
+	log.Info("listening on port:", *bind)
 	if err := http.ListenAndServe(*bind, nil); err != nil {
 		log.Fatalf("error starting server: %s", err)
 	}
 }
 
 func probeHandler(w http.ResponseWriter, r *http.Request) {
+
 	var params = r.URL.Query()
 	var target = params.Get("target")
 	var registry = prometheus.NewRegistry()
 	var start = time.Now()
 	var expiryGauge = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "domain_expiry_days",
-		Help: "time in days until the domain expires",
+		Name: "probe_dns_domain_expiry",
+		Help: "Date when the domain expires",
 	})
 	var probeDurationGauge = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "probe_duration_seconds",
-		Help: "Returns how long the probe took to complete in seconds",
+		Name: "probe_dns_duration_seconds",
+		Help: "Duration of whois request in seconds",
 	})
-	registry.MustRegister(expiryGauge)
+	var probeSuccessGauge = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "probe_dns_success",
+		Help: "Displays whether or not the probe was a success",
+	})
+	registry.Register(expiryGauge)
 	registry.MustRegister(probeDurationGauge)
+	registry.MustRegister(probeSuccessGauge)
+	probeSuccessGauge.Set(0)
 	if target == "" {
 		http.Error(w, "target parameter is missing", http.StatusBadRequest)
 		return
 	}
-	req, err := whois.NewRequest(target)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
+	record, found := storage.Get(target)
+	if found {
+		log.Info("found cache entory for domain ", target)
+		expiryGauge.Set(record.(float64))
+		probeSuccessGauge.Set(1)
+	} else {
+		req, err := whois.NewRequest(target)
+		if err != nil {
+			log.Error("error processing domain ", target, "\n", err.Error())
+		} else {
+			resp, err := whois.DefaultClient.Fetch(req)
+			if err != nil {
+				log.Error("error processing domain ", target, "\n", err.Error())
+			} else {
+				date, err := extractDate(string(resp.Body))
+				if err != nil {
+					log.Error("error processing domain ", target, "\n", err.Error())
+				} else {
+					expiryGauge.Set(float64(date.Unix()))
+					storage.Set(target, float64(date.Unix()), cache.DefaultExpiration)
+					probeSuccessGauge.Set(1)
+				}
+			}
+		}
 	}
-	resp, err := whois.DefaultClient.Fetch(req)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	days, err := extractDays(string(resp.Body))
-	if err != nil {
-		http.Error(w, fmt.Sprintf("%s: %s", target, err.Error()), http.StatusInternalServerError)
-		return
-	}
-	expiryGauge.Set(days)
 	probeDurationGauge.Set(time.Since(start).Seconds())
 	promhttp.HandlerFor(registry, promhttp.HandlerOpts{}).ServeHTTP(w, r)
 }
 
-func extractDays(body string) (float64, error) {
+func extractDate(body string) (time.Time, error) {
 	var result = re.FindStringSubmatch(body)
 	if len(result) < 2 {
-		return 0, fmt.Errorf("could not parse whois response: %s", body)
+		return time.Time{}, fmt.Errorf("could not parse whois response: %s", body)
 	}
 	var dateStr = strings.TrimSpace(result[2])
 	for _, format := range formats {
 		if date, err := time.Parse(format, dateStr); err == nil {
-			var days = math.Floor(time.Until(date).Hours() / 24)
-			return days, nil
+			return date, nil
 		}
 	}
-	return 0, fmt.Errorf("could not parse date: %s", dateStr)
+	return time.Time{}, fmt.Errorf("could not parse date: %s", dateStr)
 }

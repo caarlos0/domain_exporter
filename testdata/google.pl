78f2eb0f3e07cf7b925ec81b993bec4c23e2b570
kirillmandrygin <kirillmandrygin@itsyndicate.org>
2018-08-26 13:24:11 +0500

    feat: add tests

---
+++ testdata/google.pl
@@ -0,0 +1,30 @@
+ 
+DOMAIN NAME:           google.pl 
+registrant type:       organization 
+nameservers:           ns1.google.com.  
+                       ns2.google.com.  
+                       ns3.google.com.  
+                       ns4.google.com.  
+created:               2002.09.19 13:00:00 
+last modified:         2018.08.17 11:29:23 
+renewal date:          2019.09.18 14:00:00 
+ 
+option created:        2017.10.14 09:30:46 
+option expiration date:       2020.10.14 09:30:46 
+ 
+dnssec:                Unsigned 
+ 
+ 
+REGISTRAR: 
+Markmonitor, Inc.
+3540 East Longwing Lane, Suite 300
+Meridian, Idaho 83646
+United States
++1.2083895740
+ccops@markmonitor.com 
+ 
+WHOIS database responses: http://www.dns.pl/english/opiskomunikatow_en.html 
+ 
+WHOIS displays data with a delay not exceeding 15 minutes in relation to the .pl Registry system 
+Registrant data available at http://dns.pl/cgi-bin/en_whois.pl
+

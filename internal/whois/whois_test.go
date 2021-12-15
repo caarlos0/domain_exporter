080fa21d1f43a80da395c8cf32ba1251fafe3a69
Nontawat Numor <nontawat.nu@easysunday.com>
2021-12-15 22:45:47 +0700

    Change domain to google.co.th :tea:

--- internal/whois/whois_test.go
+++ internal/whois/whois_test.go
@@ -35,7 +35,7 @@ func TestWhoisParsing(t *testing.T) {
 		{domain: "мвд.рф", err: ""},
 		{domain: "МВД.РФ", err: ""},
 		{domain: "GOOGLE.RS", err: ""},
-		{domain: "thnic.co.th", err: ""},
+		{domain: "google.co.th", err: ""},
 	} {
 		tt := tt
 		t.Run(tt.domain, func(t *testing.T) {
